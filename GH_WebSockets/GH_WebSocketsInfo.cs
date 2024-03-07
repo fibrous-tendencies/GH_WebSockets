95a14512db27466a8883bbc22cb9dcd663f00343
Adam Burke <adam@allopoiesis.com>
2024-03-07 12:17:39 -0500

    Create repo.

--- GH_WebSockets/GH_WebSocketsInfo.cs
+++ GH_WebSockets/GH_WebSocketsInfo.cs
@@ -13,14 +13,14 @@ namespace GH_WebSockets
         //public override Bitmap Icon => null;
 
         //Return a short string describing the purpose of this GHA library.
-        public override string Description => "";
+        public override string Description => "Utility for creating a WebSocket client inside grasshopper for send and receiving data with an external server.";
 
         public override Guid Id => new Guid("4ba3f2e9-36a4-4545-97fc-a904b176e4c2");
 
         //Return a string identifying you or your company.
-        public override string AuthorName => "";
+        public override string AuthorName => "Adam Burke";
 
         //Return a string representing your preferred contact details.
-        public override string AuthorContact => "";
+        public override string AuthorContact => "aburke3@mit.edu";
     }
 }
