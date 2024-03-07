93c7dc91e9c277a9eff07fe53845aec3e0988d05
Adam Burke <83083545+fibrous-tendencies@users.noreply.github.com>
2024-03-07 13:51:11 -0500

    Update python_echo_server.py

--- Examples/python_echo_server.py
+++ Examples/python_echo_server.py
@@ -1,1 +1,22 @@
+import websockets
+import asyncio
 
+async def handler(websocket):
+    while True:
+        message = await websocket.recv()
+        try:
+            print(message)
+
+            await websocket.send(message)
+        
+        except Exception as e:
+            print(e)
+            print("Error in message")
+            continue
+        
+async def main():
+    async with websockets.serve(handler, "127.0.0.1", 2000) as ws:
+        await asyncio.Future() # run forever
+
+
+asyncio.run(main())
